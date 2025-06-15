43f6873d4920ab46061c6305d4049adc81309f2e
Ys-Nyang <did20031@naver.com>
2025-06-16 05:07:58 +0900

    이미지 수정

--- src/components/ContactSection.tsx
+++ src/components/ContactSection.tsx
@@ -10,21 +10,21 @@ export const ContactSection = () => {
       <div className="contact-list">
         <div className="contact-item">
           <div className="contact-icon">
-            <img src="/icon.svg" alt="icon" />
+            <img src="/insta.svg" alt="insta" />
           </div>
           <div className="contact-text">ys_nyang</div>
         </div>
 
         <div className="contact-item">
           <div className="contact-icon">
-            <img src="/icon-2.svg" alt="icon2" />
+            <img src="/email.svg" alt="email" />
           </div>
           <div className="contact-text">did20031@naver.com</div>
         </div>
 
         <div className="contact-item">
           <div className="contact-icon">
-            <img src="/icon-3.svg" alt="icon3" />
+            <img src="/discord.svg" alt="discord" />
           </div>
           <div className="contact-text">discord invite : ys_nyang</div>
         </div>
